0267736fefc653df04380dd446952f9d488d6d5e
Pavan B <83314733+ValiantWind@users.noreply.github.com>
2022-09-20 22:39:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+  scanner.addListener('scan', function (content) {
+      alert(content);
+});
+
+// function startScanner() {
+Instascan.Camera.getCameras().then(function (cameras) {
+    if (cameras.length > 0) {
+        scanner.start(cameras[0]);
+    } else {
+        console.error('No cameras found.');
+     }
+}).catch(function (e) {
+  console.error(e);
+});
+        //}
+        // function stopScanner() {
+        //     scanner.stop();
+        // }
